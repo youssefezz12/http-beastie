0aff4d0010ec85b803874d6286633cbf06b60912
youssefezz12 <youssefezz@gmail.com>
2026-05-08 20:06:14 +0300

    Implement file reading for HTTP server

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,9 +1,31 @@
 #include "utils.hpp"
 
+#include <fstream>
+#include <string>
+
 namespace beast = boost::beast;
 namespace http = beast::http;
 
-http::response<http::string_body> handle_request(const http::request<http::string_body>& req) {
+std::string read_file(const std::string& path) {
+  std::ifstream myfile(path);
+
+  if (!myfile.is_open()) {
+    return "";
+  }
+
+  std::string line;
+  std::string content;
+
+  while (std::getline(myfile, line)) {
+    content += line + "\n";
+  }
+
+  return content;
+}
+
+http::response<http::string_body> handle_request(
+    const http::request<http::string_body>& req) {
+
   http::response<http::string_body> res;
 
   res.version(req.version());
@@ -13,19 +35,28 @@ http::response<http::string_body> handle_request(const http::request<http::strin
   res.set(http::field::content_type, "text/html");
 
   if (req.method() == http::verb::get) {
+
     if (req.target() == "/") {
+
       res.result(http::status::ok);
-      res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
+      res.body() = read_file("../static/index.html");
+
     } else {
+
       res.result(http::status::not_found);
-      res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+      res.body() =
+          "<h1 style=\"text-align: center;\">404 Not Found</h1>";
     }
+
   } else {
+
     res.result(http::status::method_not_allowed);
     res.set(http::field::allow, "GET");
-    res.body() = "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
+    res.body() =
+        "<h1 style=\"text-align: center;\">405 Method Not Allowed</h1>";
   }
 
   res.prepare_payload();
+
   return res;
 }
