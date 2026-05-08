5f501d9f49e1057437a26b14a5c0f20b1c009418
youssefezz12 <youssefezz@gmail.com>
2026-05-08 19:47:03 +0300

    implementation of function that returns a string

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,3 +3,4 @@
 #include <boost/beast/http.hpp>
 
 boost::beast::http::response<boost::beast::http::string_body> handle_request(const boost::beast::http::request<boost::beast::http::string_body>& req);
+std::string read_file(const std::string& path);
